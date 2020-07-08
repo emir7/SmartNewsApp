8e44fdfa463acdeb6cf374c08ec1e513d021ee96
emirhasanbegovic <eh7718@student.uni-lj.si>
2020-07-08 19:14:37 +0200

    Updated tutorial

--- machine-learning/android/src/main/java/com/ml/plugin/Constants.java
+++ machine-learning/android/src/main/java/com/ml/plugin/Constants.java
@@ -2,6 +2,7 @@ package com.ml.plugin;
 
 public class Constants {
 
+    // 163.172.169.249
     public static final String SERVER_IP = "http://93.103.215.63:9082";
     public static final String DEBUG_VAR = "EO_ME";
 
